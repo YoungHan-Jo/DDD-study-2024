2a3211c696ff97273862c5b93f0aa9780a8cafe0
YoungHan Jo <verin4494@gmail.com>
2024-09-14 15:12:39 +0900

    start

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { OrderController } from './modules/order/controller/order.controller';
+import { OrderModule } from './modules/order/order.module';
+
+@Module({
+  imports: [OrderModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
